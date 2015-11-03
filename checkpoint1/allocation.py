62df9ac49286a11d0cb8d68d479292765ff1065a
Eric G <eric.gichuri@andela.com>
2015-11-03 14:28:48 +0300

    Change folder structure
    Create a separate folder for files storing the data

--- checkpoint1/allocation.py
+++ checkpoint1/allocation.py
@@ -9,29 +9,29 @@ class Allocation(object):
 	def allocate(self, room_type, member):
 		#Check if there are available rooms
 		space = room.Room()
-		availableRooms = space.available(room_type)
-		if len(availableRooms) <= 0:
+		available_rooms = space.available(room_type)
+		if len(available_rooms) <= 0:
 			if room_type == "O":
 				print "Sorry, there are no more available office spaces"
 			elif room_type == "L":
 				print "Sorry, there are no more available living spaces"
 		else:
-			rand_room = random.choice(availableRooms)
+			rand_room = random.choice(available_rooms)
 			space.addMember(rand_room, room_type, member)
 
 
 	def allocateAll(self, user_type):
 		#Get unallocated users
 		users = user.User()
-		toallocate = users.unallocated(user_type)
+		to_allocate = users.unallocated(user_type)
 
 		#Allocate office spaces
-		for member in toallocate["office"]:
+		for member in to_allocate["office"]:
 			self.allocate("O", member)
 
 		#Allocate living spaces only to fellows
 		if user_type == "F":
-			for member in toallocate["living"]:
+			for member in to_allocate["living"]:
 				user_details =  users.getUser(member)
 				if user_details["accomodation"] == "Y":
 					self.allocate("L", member)
