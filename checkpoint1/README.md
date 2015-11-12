049488429bbca28938f08231f342e0766f191764
Eric G <eric.gichuri@andela.com>
2015-11-12 12:05:50 +0300

    Merge with Develop
    Squashed commit of the merge with the Develop branch after checkpoint review changes
    
        Create additional tests to cater for methods that write data to files
        Correct comments
        Adhere to DocString conventions

--- checkpoint1/README.md
+++ checkpoint1/README.md
@@ -4,7 +4,7 @@ A room allocation system for (Amity)
 ## Usage Instructions:
 * Install Python
 * Download or clone the source files
-* run `python main.py` from the terminal inside "checkpoint1"
+* run `python main.py` from the terminal inside `checkpoint1`
 
 ### Adding users
 Users can be added either by entering data manually or using a text file.
@@ -12,9 +12,9 @@ The text file should be in the format:
 
 `full names \t user_type \t accomodation`
 
-The user type should be either "F" for Fellow or "S" for staff
+The user type should be either `F` for Fellow or `S` for staff
 
-Accomodation should be either "Y" or "N" for fellows and blank or "N/A" for staff
+Accomodation should be either `Y` or `N` for fellows and blank or `N/A` for staff
 
 ### Adding rooms
 Rooms can be added either by entering data manually or using a text file.
@@ -23,9 +23,9 @@ The text file should be in the format:
 
 `room name \t room_type`
 
-The room type should be either "L" for living space or "O" for office space
+The room type should be either `L` for living space or `O` for office space
 
-*Both the user and room details files should be placed inside the "data" directory.*
+*Both the user and room details files should be placed inside the `data` directory.*
 *Sample files are included*
 
 ###Room Allocation
@@ -44,7 +44,7 @@ One can view:
 ### Tests
 Run `pip install nose`
 
-Run `nosetests` while in the checkpoints1 directory
+Run `nosetests` while in the `checkpoints1` directory
 
 ## Known Issues
  - The Exit functionality doesn't run after any other option is selected
--- checkpoint1/user.py
+++ checkpoint1/user.py
@@ -2,164 +2,190 @@ import json
 import random
 import os
 
+
 class User(object):
+    '''
+    The User class defines the data and methods used in the system 
+    that are related to users
+    '''
 
-	def __init__(self):
-		self.user_type = ""
-		self.user_name = ""
-		self.accomodation = ""
-		self.user_id = ""
-		self.f_path = os.path.dirname(os.path.abspath(__file__))
-		self.user_data = os.path.join(self.f_path, "data/users.json")
-		self.room_data = os.path.join(self.f_path, "data/rooms.json")
+    def __init__(self, d_path=os.path.dirname(os.path.abspath(__file__))):
+        self.user_type = ""
+        self.user_name = ""
+        self.accomodation = ""
+        self.user_id = ""
+        self.d_path = d_path
+        self.user_data = os.path.join(self.d_path, "data/users.json")
+        self.room_data = os.path.join(self.d_path, "data/rooms.json")
 
-	#Save user to data file
-	def saveUser(self):
-		tosave = {"username" : self.user_name,  "accomodation" : self.accomodation}
-		with open(self.user_data, "r+") as data_file:
-			data = json.load(data_file)
-			user_inst = User()
-			user_names = []
-			if self.user_type == "F":
-				fellow_list =  user_inst.users("F")
-				for each_user in fellow_list:
-					user_details = self.getUser(each_user)
-					user_names.append(user_details["username"])
-				if self.user_name not in user_names:
-					data["users"]["fellows"][self.user_id] = tosave
-					print "Member " + self.user_name +" added to the system"
-				else:
-					print "The member " + self.user_name +" is currently in the system"
-			elif self.user_type == "S":
-				staff_list =  user_inst.users("S")
-				for each_user in staff_list:
-					user_details = self.getUser(each_user)
-					user_names.append(user_details["username"])
-				if self.user_name not in user_names:
-					data["users"]["staff"][self.user_id] = tosave
-					print "Member " + self.user_name +" added to the system"
-				else:
-					print "The member " + self.user_name +" is currently in the system"
-			data_file.seek(0)
-			data_file.write(json.dumps(data, indent=4, sort_keys=True))
+    def saveUser(self):
+        '''
+        Save user to data file
+        '''
+        tosave = {"username": self.user_name, "accomodation": self.accomodation}
+        with open(self.user_data, "r+") as data_file:
+            data = json.load(data_file)
+            user_names = []
+            if self.user_type == "F":
+                fellow_list = self.users("F")
+                for each_user in fellow_list:
+                    user_details = self.getUser(each_user)
+                    user_names.append(user_details["username"])
+                if self.user_name not in user_names:
+                    data["users"]["fellows"][self.user_id] = tosave
+                    print "Member " + self.user_name + " added to the system"
+                else:
+                    print "The member " + self.user_name + " is currently in the system"
+            elif self.user_type == "S":
+                staff_list = self.users("S")
+                for each_user in staff_list:
+                    user_details = self.getUser(each_user)
+                    user_names.append(user_details["username"])
+                if self.user_name not in user_names:
+                    data["users"]["staff"][self.user_id] = tosave
+                    print "Member " + self.user_name + " added to the system"
+                else:
+                    print "The member " + self.user_name + " is currently in the system"
+            data_file.seek(0)
+            data_file.write(json.dumps(data, indent=4, sort_keys=True))
 
-	#Add user data manually
-	def addUser(self):
-		user_type = raw_input("User Type? Enter S for staff and F for fellow: \n").upper()
 
-		while user_type != "F" and user_type != "S":
-			user_type = raw_input("Try again. Enter S for staff and F for fellow: \n").upper()
+    def addUser(self):
+        '''
+        Add user data manually
+        '''
+        user_type = raw_input(
+            "User Type? Enter S for staff and F for fellow: \n").upper()
 
-		name = raw_input("Enter name: \n").upper()
+        while user_type != "F" and user_type != "S":
+            user_type = raw_input(
+                "Try again. Enter S for staff and F for fellow: \n").upper()
 
-		if user_type == "F":
-			accomodation = raw_input("Does the fellow need living space? (Y or N): \n").upper()
-			while accomodation != "Y" and accomodation != "N":
-				accomodation = raw_input("Try again. Allocate living space? (Y or N): \n").upper()
+        name = raw_input("Enter name: \n").upper()
 
-		elif user_type == "S":
-			accomodation = "N/A"
+        if user_type == "F":
+            accomodation = raw_input(
+                "Does the fellow need living space? (Y or N): \n").upper()
+            while accomodation != "Y" and accomodation != "N":
+                accomodation = raw_input(
+                    "Try again. Allocate living space? (Y or N): \n").upper()
 
-		self.user_type = user_type
-		self.user_name = name
-		self.accomodation = accomodation
-		self.user_id = self.user_name[0:3].lower() + str((random.randint(10,100)))
+        elif user_type == "S":
+            accomodation = "N/A"
 
-		self.saveUser()
+        self.user_type = user_type
+        self.user_name = name
+        self.accomodation = accomodation
+        self.user_id = self.user_name[
+            0:3].lower() + str((random.randint(10, 100)))
 
-	#Return a list of unallocated users
-	def unallocated(self, user_type):
-		office = []
-		living = []
-		with open(self.user_data, 'r') as f:
-			data = json.load(f)
-			if user_type == "F":
-				users = data["users"]["fellows"]
-				for key in users:
-					if "office" not in users[key] or users[key]["office"] == "":
-						office.append(key)
+        self.saveUser()
 
-					if "living" not in users[key] or users[key]["living"] == "":
-						living.append(key)
-				userlist = {"office": office, "living" : living}
+    def unallocated(self, user_type):
+        '''
+        Returns a list of unallocated users
+        '''
+        office = []
+        living = []
+        with open(self.user_data, 'r') as f:
+            data = json.load(f)
+            if user_type == "F":
+                users = data["users"]["fellows"]
+                for key in users:
+                    if "office" not in users[key] or users[key]["office"] == "":
+                        office.append(key)
 
-			elif user_type == "S":
-				users = data["users"]["staff"]
-				for key in users:
-					if "office" not in users[key] or users[key]["office"] == "":
-						office.append(key)
+                    if "living" not in users[key] or users[key]["living"] == "":
+                        living.append(key)
+                userlist = {"office": office, "living": living}
 
-				userlist = {"office": office}
-		return userlist
+            elif user_type == "S":
+                users = data["users"]["staff"]
+                for key in users:
+                    if "office" not in users[key] or users[key]["office"] == "":
+                        office.append(key)
 
-	#View users yet to be allocated either a living space or an office space
-	def view_unallocated(self, user_type):
-		os.system('clear')
-		to_print = self.unallocated(user_type)
-		if user_type == "F":
-			print "FELLOWS"
-			print "********************************************************************"
-			print "To Allocate Living Space"
-			living = to_print["living"]
-			for item in living:
-				user_details = self.getUser(item)
-				if user_details["accomodation"] == "Y":
-					print user_details["username"]
-			print "\n"
-		if user_type == "S":
-			print "STAFF"
-			print "********************************************************************"
-		print "To Allocate Office Space"
-		office = to_print["office"]
-		for member in office:
-			user_details = self.getUser(member)
-			print user_details["username"]
+                userlist = {"office": office}
+        return userlist
 
-	#Display all users
-	def listUsers(self, user_type):
-		userlist = "Name\t User Type \t\t Living Space\n"
-		with open(self.user_data, 'r') as f:
-			data = json.load(f)
-			if user_type == "F":
-				users = data["users"]["fellows"]
-				utype = "Fellow"
-			elif user_type == "S":
-				users = data["users"]["staff"]
-				utype = "Staff"
+    def view_unallocated(self, user_type):
+        '''
+        View users yet to be allocated either a living space or an office space
+        '''
+        os.system('clear')
+        to_print = self.unallocated(user_type)
+        if user_type == "F":
+            print "FELLOWS"
+            print "********************************************************************"
+            print "To Allocate Living Space"
+            living = to_print["living"]
+            for item in living:
+                user_details = self.getUser(item)
+                if user_details["accomodation"] == "Y":
+                    print user_details["username"]
+            print "\n"
+        if user_type == "S":
+            print "STAFF"
+            print "********************************************************************"
+        print "To Allocate Office Space"
+        office = to_print["office"]
+        for member in office:
+            user_details = self.getUser(member)
+            print user_details["username"]
 
-		for key in users:
-			userlist = userlist + users[key]["username"] + "\t\t" + user_type + "\t" +  users[key]["accomodation"] + "\t"
+    def listUsers(self, user_type):
+        '''
+        Display all users
+        '''
+        userlist = "Name\t User Type \t\t Living Space\n"
+        with open(self.user_data, 'r') as f:
+            data = json.load(f)
+            if user_type == "F":
+                users = data["users"]["fellows"]
+                utype = "Fellow"
+            elif user_type == "S":
+                users = data["users"]["staff"]
+                utype = "Staff"
 
-			userlist = userlist + "\n"
-		return userlist
+        for key in users:
+            userlist = userlist + \
+                users[key]["username"] + "\t\t" + user_type + \
+                "\t" + users[key]["accomodation"] + "\t"
 
-	#Return a list of all users of a given type (Either fellow or staff)
-	def users(self, user_type):
-		user_ids = []
-		with open(self.user_data, 'r') as f:
-			data = json.load(f)
-			if user_type == "F":
-				users = data["users"]["fellows"]
-			elif user_type == "S":
-				users = data["users"]["staff"]
+            userlist = userlist + "\n"
+        return userlist
 
-		for key in users:
-			user_ids.append(key)
+    def users(self, user_type):
+        '''
+        Return a list of all users of a given type (Either fellow or staff)
+        '''
+        user_ids = []
+        with open(self.user_data, 'r') as f:
+            data = json.load(f)
+            if user_type == "F":
+                users = data["users"]["fellows"]
+            elif user_type == "S":
+                users = data["users"]["staff"]
 
-		return user_ids
+        for key in users:
+            user_ids.append(key)
 
-	#Get details of a given user
-	def getUser(self, user_id):
-		userlist = {}
-		with open(self.user_data, 'r') as f:
-			data = json.load(f)
-			users1 = data["users"]["fellows"]
-			users2 = data["users"]["staff"]
-		userlist["userID"] = user_id
-		if user_id in users1:
-			for each, value in users1[user_id].iteritems():
-				userlist[each] = value
-		if user_id in users2:
-			for each, value in users2[user_id].iteritems():
-				userlist[each] = value
-		return userlist
+        return user_ids
+
+    def getUser(self, user_id):
+        '''
+        Get details of a given user
+        '''
+        userlist = {}
+        with open(self.user_data, 'r') as f:
+            data = json.load(f)
+            users1 = data["users"]["fellows"]
+            users2 = data["users"]["staff"]
+        userlist["userID"] = user_id
+        if user_id in users1:
+            for each, value in users1[user_id].iteritems():
+                userlist[each] = value
+        if user_id in users2:
+            for each, value in users2[user_id].iteritems():
+                userlist[each] = value
+        return userlist
